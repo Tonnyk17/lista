5ced009dc1cc59288f85a5f8d6f876cd3f9c762e
Tonyk <tonykurosaki117@gmail.com>
2021-10-04 00:23:33 -0500

    busqueda de elemento

--- lista.py
+++ lista.py
@@ -10,6 +10,7 @@ class nodo:
         self._inicio=None
 class listaEnlazada:
     _raiz=None
+    _q=None
     _longitud=0
     def __init__(self):
         self._raiz=None
@@ -28,13 +29,15 @@ class listaEnlazada:
             siguiente._next=nuevo_nodo
 
     def insertarInicio(self,nuevo_nodo):
-            self._raiz=nuevo_nodo 
+            nuevo_nodo._next =self._raiz 
+            self._raiz = nuevo_nodo
 
     def imprimir(self):
         siguiente=self._raiz
         while siguiente != None:
-            siguiente=siguiente._next
             print(siguiente._dato)
+            siguiente=siguiente._next
+            
 
     def tamano(self):
         print("Longitud de la lista:",self._longitud)
@@ -68,30 +71,94 @@ class listaEnlazada:
             self._longitud -=1
 
     def insertarPosicion(self,nuevo_nodo,posicion):
-        if self._raiz ==None:
-            self._raiz=nuevo_nodo
+        if posicion ==1:
+            nuevo_nodo._next = self._raiz
+            self._raiz = nuevo_nodo
+        i = 1
+        n = self._raiz
+        while posicion - 1 > i and n != None:
+            n = n._next
+            i = i+1
+        if n == None:
+            return False
+        else:
+            nuevo_nodo._next = n._next
+            n._next = nuevo_nodo
+        
+    def eliminarInicio(self):
+        if self._raiz == None:
+            return False
+        else:
+            self._raiz = self._raiz._next        
+            self._longitud -= 1
+
+    def invertir(self):
+        if self._raiz == None:
+            return False
+        else:
+            siguiente = anterior = None
+            aux = self._raiz
+            while aux:
+                siguiente = aux._next
+                aux._next = anterior
+                anterior = aux
+                aux = siguiente
+            self._raiz = anterior
+
+    def burbuja(self):
+        if self._raiz == None:
+            return False
+        else:
+           siguiente = self._raiz
+           while siguiente._next != None:
+               q = siguiente._next
+               while q != None:
+                   if siguiente._dato > q._dato:
+                       aux = siguiente._dato
+                       siguiente._dato = q._dato
+                       q._dato= aux
+
+    def buscar(self,valor):
+        if self._raiz == None:
+            return False
         else:
             siguiente = self._raiz
-            while siguiente._next == posicion:
+            while siguiente != None:
+                if siguiente._dato == valor._dato:
+                    print('Valor encontrado')
+                    return True
                 siguiente = siguiente._next
-                siguiente._next = nuevo_nodo
+            print('Valor no encontrado')
+           
+    def reemplazar(self,valor,posicion):
+        if self._raiz == None:
+            return False
+        else:
+            if posicion >= 0 and self._longitud > posicion:
+                siguiente = self._raiz
 
+                for i in range(posicion):
+                    siguiente = siguiente._next
+                siguiente._dato =valor._dato
+                
+                
         
-
-        
-            
-
 n1=nodo(5)
 n2=nodo(6)
 n3=nodo(29)
-n4=nodo(19)
+n4=nodo(123)
 
 listaSimple = listaEnlazada()
 listaSimple.insertar(n1)
 listaSimple.insertar(n2)
 listaSimple.insertar(n3)
-#listaSimple.insertarPosicion(n4,n2)
+#listaSimple.insertarInicio(n4)
+#listaSimple.eliminarInicio()
+#listaSimple.insertarPosicion(n4,2)
 #listaSimple.eliminarSeleccion(n2)
 #listaSimple.eliminarFinal()
+#listaSimple.burbuja()
+listaSimple.reemplazar(n4,1)
+#listaSimple.invertir()
 listaSimple.imprimir()
 listaSimple.tamano()
